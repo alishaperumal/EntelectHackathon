79c7e8a51b57697a87f9240760aa7dcddb0c8b85
alishaperumal <alishaperumal11@gmail.com>
2026-04-18 10:29:02 +0200

    initial js setup

---
+++ Level1_Solution/Level1Solution.js
@@ -0,0 +1,11 @@
+// Level 1 Solution
+
+// Initialize your variables and functions here
+function main() {
+    console.log("Level 1 Solution started");
+    
+    // Add your code here
+}
+
+// Run the main function
+main();
